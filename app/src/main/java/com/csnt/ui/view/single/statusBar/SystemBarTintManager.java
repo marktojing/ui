de8881555de6a98d357c9206f5f96f5aab7ac1b1
jinglei <965334541@qq.com>
2020-06-24 10:59:28 +0800

    尝试修复bug2

--- app/src/main/java/com/csnt/ui/view/single/statusBar/SystemBarTintManager.java
+++ app/src/main/java/com/csnt/ui/view/single/statusBar/SystemBarTintManager.java
@@ -84,6 +84,7 @@ public class SystemBarTintManager {
      * @param activity
      *            The host activity.
      */
+    @SuppressLint("ResourceType")
     @TargetApi(19)
     public SystemBarTintManager(Activity activity) {
 
